fd67af2b7c1715fbc6df1502df7e1644032a3c13
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-07-31 10:15:44 -0300

    ejemplo

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import burga from './assets/burga.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.letra}>MC Donalds</Text>
+      <Image source={burga} style={styles.hamburguesa} />
+      <Button color='black' title='Comprar'></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  boton:{
+    color:'yellow',
+    title:'Comprar',
+  },
+  letra:{
+    color:'yellow',
+    fontSize: 50,
+  },
+  hamburguesa:{
+    width: 300,
+    height: 300,
+  }
 });
