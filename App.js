45407701eb78bc8d51ba66b172f1621d1de5afe8
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-01 09:00:50 -0300

    ejemplo clase 2

--- App.js
+++ App.js
@@ -1,23 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { setStatusBarBackgroundColor, StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, Image,ImageBackground } from 'react-native';
 import burga from './assets/burga.png';
-
+import fondo from './assets/fondo.png'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.letra}>MC Donalds</Text>
-      <Image source={burga} style={styles.hamburguesa} />
-      <Button color='black' title='Comprar'></Button>
+      
+      <ImageBackground source={fondo} resizeMode='cover' style={styles.background}>
+        <Text style={styles.letra}>MC Donalds</Text>
+        <Image source={burga} style={styles.hamburguesa} />
+        <Button color='black' title='Comprar'></Button>
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    display:'flex',
     flex: 1,
-    backgroundColor: 'red',
-    alignItems: 'center',
     justifyContent: 'center',
   },
   boton:{
@@ -27,9 +30,23 @@ const styles = StyleSheet.create({
   letra:{
     color:'yellow',
     fontSize: 50,
+    backgroundColor: '#FF000090',
+    width: "80%",
+    textAlign:'center',
+    margin: 'auto'
+    
   },
   hamburguesa:{
     width: 300,
     height: 300,
+    alignSelf:'center'
+  },
+  background:{
+    flex:1,
+    justifyContent:'center',
+    display:'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+    
   }
 });
