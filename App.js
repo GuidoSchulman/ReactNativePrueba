8e281ec56861da2ecc07d0bbea5950ca344c5ad4
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-07 09:54:35 -0300

    -

--- App.js
+++ App.js
@@ -1,52 +1,101 @@
-import { setStatusBarBackgroundColor, StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Image,ImageBackground } from 'react-native';
-import burga from './assets/burga.png';
-import fondo from './assets/fondo.png'
+import React from "react";
+import { setStatusBarBackgroundColor, StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Image,
+  ImageBackground,
+  Alert,
+  SafeAreaView,
+  TextInput,
+  Pressable,
+} from "react-native";
+
+import burga from "./assets/burga.png";
+import fondo from "./assets/fondo.png";
+
+
 export default function App() {
+  const [hamburguesa, handleHamburguesaFav] = React.useState("");
+
+
   return (
-    <View style={styles.container}>
-      
-      <ImageBackground source={fondo} resizeMode='cover' style={styles.background}>
+    <SafeAreaView style={styles.container}>
+      <ImageBackground
+        source={fondo}
+        resizeMode="cover"
+        style={styles.background}
+      >
         <Text style={styles.letra}>MC Donalds</Text>
         <Image source={burga} style={styles.hamburguesa} />
-        <Button color='black' title='Comprar'></Button>
+        <Pressable style={styles.button} onPress={()=>Alert.alert('Compra realizada')}>
+        <Text style={styles.text}>Comprar</Text>
+        </Pressable>
       </ImageBackground>
       <StatusBar style="auto" />
-    </View>
-    
+      <SafeAreaView style={[styles.container, styles.container2]}>
+      <SafeAreaView  style={{backgroundColor: 'lightblue', flex: 5}}>
+      <Text>Ingrese condimentos</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="ingrese..."
+          onChangeText={handleHamburguesaFav}
+          value={hamburguesa}
+        />
+        <Text > Tu hamburguesa tiene: {hamburguesa}</Text>
+        </SafeAreaView>
+      </SafeAreaView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    display:'flex',
+    display: "flex",
     flex: 1,
+    justifyContent: "center",
+  },
+  container2: {
+    margin: 5,
+  },
+
+  button: {
+    alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    backgroundColor: 'black',
   },
-  boton:{
-    color:'yellow',
-    title:'Comprar',
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
   },
-  letra:{
-    color:'yellow',
+
+  letra: {
+    color: "yellow",
     fontSize: 50,
-    backgroundColor: '#FF000090',
+    backgroundColor: "#FF000090",
     width: "80%",
-    textAlign:'center',
-    margin: 'auto'
-    
+    textAlign: "center",
+    margin: "auto",
   },
-  hamburguesa:{
+  input: { height: 40, borderWidth: 2, padding: 1 },
+  hamburguesa: {
     width: 300,
     height: 300,
-    alignSelf:'center'
+    alignSelf: "center",
   },
-  background:{
-    flex:1,
-    justifyContent:'center',
-    display:'flex',
-    justifyContent: 'center',
-    alignItems: 'center'
-    
-  }
+  background: {
+    flex: 1,
+    justifyContent: "center",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
