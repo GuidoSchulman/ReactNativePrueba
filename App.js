a41972b7d96e650a15fb141d62bb1e506d26d431
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-08 08:22:56 -0300

    .

--- App.js
+++ App.js
@@ -23,8 +23,7 @@ export default function App() {
   const [telefono, handleTelefono] = React.useState("");
   const [email, handleEmail] = React.useState("");
   const [clave, handleClave] = React.useState("");
-  
-
+  const onPress = () =>console.log(nombre + " " + telefono + " " + email + " " + clave);
   return (
     <SafeAreaView style={styles.container}>
       <ImageBackground
@@ -34,44 +33,65 @@ export default function App() {
       >
         <Text style={styles.letra}>MC Donalds</Text>
         <Image source={burga} style={styles.hamburguesa} />
-        <Pressable style={styles.button} onPress={()=>Alert.alert('Compra realizada')}>
-        <Text style={styles.text}>Comprar</Text>
+        <Pressable
+          style={styles.button}
+          onPress={() => Alert.alert("Compra realizada")}
+        >
+          <Text style={styles.text}>Comprar</Text>
         </Pressable>
       </ImageBackground>
       <StatusBar style="auto" />
       <SafeAreaView style={[styles.container, styles.container2]}>
-      <SafeAreaView  style={{backgroundColor: 'lightblue', flex: 5}}>
-      <Text > Para confirmar la compra ingresa los siguientes datos</Text>
+        <SafeAreaView style={{ backgroundColor: "lightblue", flex: 5 }}>
+          <Text> Para confirmar la compra ingresa los siguientes datos</Text>
 
-        <TextInput
-          style={styles.input}
-          placeholder="ingrese nombre..."
-          onChangeText={handleNombre}
-          value={nombre}
-        />
-        <Text ></Text>
-        <TextInput
-          style={styles.input}
-          placeholder="ingrese Telefono..."
-          onChangeText={handleTelefono}
-          value={telefono}
-        />
-        <Text ></Text>
-        <TextInput
-          style={styles.input}
-          placeholder="ingrese Email..."
-          onChangeText={handleEmail}
-          value={email}
-        />
-        <Text ></Text>
-        <TextInput
-          style={styles.input}
-          placeholder="ingrese clave..."
-          onChangeText={handleClave}
-          value={clave}
-        />
-        <Text ></Text>
+          <TextInput
+            style={styles.input}
+            placeholder="ingrese nombre..."
+            onChangeText={handleNombre}
+            value={nombre}
+          />
+          <Text></Text>
+          <TextInput
+            style={styles.input}
+            placeholder="ingrese Telefono..."
+            onChangeText={handleTelefono}
+            value={telefono}
+          />
+          <Text></Text>
+          <TextInput
+            style={styles.input}
+            placeholder="ingrese Email..."
+            onChangeText={handleEmail}
+            value={email}
+          />
+          <Text></Text>
+          <TextInput
+            style={styles.input}
+            placeholder="ingrese clave..."
+            onChangeText={handleClave}
+            value={clave}
+          />
+          <Text></Text>
         </SafeAreaView>
+        <View style={styles.container3}>
+          <View>
+            <Pressable
+              style={styles.button}
+              onPress={onPress}
+            >
+              <Text style={styles.text}>Mostrar</Text>
+            </Pressable>
+          </View>
+          <View>
+            <Button title="Mostrar" onPress={onPress}></Button>
+          </View>
+          <View>
+          <TouchableOpacity title="Mostrar" activeOpacity={1} onPress={onPress}>
+        <Text>Mostrar</Text>
+        </TouchableOpacity>
+          </View>
+        </View>
       </SafeAreaView>
     </SafeAreaView>
   );
@@ -86,21 +106,26 @@ const styles = StyleSheet.create({
   container2: {
     margin: 5,
   },
+  container3: {
+    flex: 3,
+    justifyContent: "center",
+    alignItems: "center"
+  },
 
   button: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     paddingVertical: 12,
     paddingHorizontal: 32,
     borderRadius: 4,
-    backgroundColor: 'black',
+    backgroundColor: "black",
   },
   text: {
     fontSize: 16,
     lineHeight: 21,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     letterSpacing: 0.25,
-    color: 'white',
+    color: "white",
   },
 
   letra: {
